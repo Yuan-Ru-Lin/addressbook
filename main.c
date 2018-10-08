6c789bdc26315cfaa2fb7d57e68de33030cbd6f7
Leon_Lin <LeonLin1997@gmail.com>
2018-10-08 17:26:01 +0800

    * main.c: Add the interface

--- main.c
+++ main.c
@@ -7,6 +7,14 @@ int main() {
     char op = 0;
 
     while (1) {
+        puts("i: 插入");
+        puts("l: 列出");
+        puts("s: 存檔");
+        puts("o: 讀檔");
+        puts("q: 結束");
+        printf("> ");
+        scanf("%s", &op);
+
         switch (op) {
         case 'i':
             break;
