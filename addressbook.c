838fb66117cc8bf6e1332758f585a47369f4c7bc
Leon_Lin <LeonLin1997@gmail.com>
2018-11-10 17:33:39 +0800

    Unitize each operations and format document

--- addressbook.c
+++ addressbook.c
@@ -7,6 +7,12 @@ typedef struct _Employee {
     char email[80];
 } Employee;
 
+void insert();
+void list();
+void save();
+void read();
+void quit();
+
 int main() {
     FILE* in = fopen("address_book.txt", "a");
     char op = 0;
@@ -21,29 +27,37 @@ int main() {
         scanf("%s", &op);
 
         switch (op) {
-        case 'i':
-            Employee temp;
-            puts("姓名: ");
-            scanf("%s", &temp.name);
-            fprintf(in, "%s\n", temp.name);
-            puts("電話: ");
-            scanf("%s", &temp.phone);
-            fprintf(in, "%s\n", temp.phone);
-            puts("Email: ");
-            scanf("%s", &temp.email);
-            fprintf(in, "%s\n", temp.email);
-            break;
-        case 'l':
-            break;
-        case 's':
-            break;
-        case 'o':
-            break;
-        case 'q':
-            break;
+            case 'i':
+                insert();
+                break;
+            case 'l':
+                list();
+                break;
+            case 's':
+                save();
+                break;
+            case 'o':
+                read();
+                break;
+            case 'q':
+                quit();
+                break;
         }
         system("clear");
     }
 
     return 0;
+}
+
+void insert() {
+    Employee temp;
+    puts("姓名: ");
+    scanf("%s", &temp.name);
+    fprintf(in, "%s\n", temp.name);
+    puts("電話: ");
+    scanf("%s", &temp.phone);
+    fprintf(in, "%s\n", temp.phone);
+    puts("Email: ");
+    scanf("%s", &temp.email);
+    fprintf(in, "%s\n", temp.email);
 }
